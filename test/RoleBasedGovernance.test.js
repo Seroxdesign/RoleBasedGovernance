566e2687332a6c37611e07bd577b457d515acf71
sky cherfa <sherifcherfa@gmail.com>
2023-06-28 05:20:54 -0400

    updated test

--- test/RoleBasedGovernance.test.js
+++ test/RoleBasedGovernance.test.js
@@ -1,47 +1,65 @@
+// Import the contract artifacts and assert library
 const RoleBasedGovernance = artifacts.require('RoleBasedGovernance');
+const truffleAssert = require('truffle-assertions');
 
 contract('RoleBasedGovernance', (accounts) => {
-  let governanceContract;
-
-  const owner = '0x45751861f0101A857c1656eBCe3173AD249A4A60';
-  const memberWithRole1 = [1, 2, 3];
-  const memberWithRole2 = [10, 21, 33];
-  const daoExpanderAddress = '0x45751861f0101A857c1656eBCe3173AD249A4A60';
+  let governance;
 
   beforeEach(async () => {
-    governanceContract = await RoleBasedGovernance.new([1, 2], [100, 200], daoExpanderAddress, {
-      from: owner,
-    });
+    governance = await RoleBasedGovernance.new([10, 21, 18], [10, 21, 18], accounts[0]);
   });
 
-  it('should allow a member with role 1 to create a proposal', async () => {
-    const metadataCID = 'proposal-metadata';
-    const startTime = Math.floor(Date.now() / 1000); // Current timestamp
-    const endTime = startTime + 3600; // 1 hour from the start time
+  it('should create a proposal', async () => {
+    const startTime = Math.floor(Date.now() / 1000);
+    const endTime = startTime + 86400; // 1 day
 
-    await governanceContract.createProposal(metadataCID, startTime, endTime, { from: memberWithRole1 });
+    const result = await governance.createProposal('Proposal 1', startTime, endTime);
+    const proposalId = result.logs[0].args.proposalId.toNumber();
 
-    const proposal = await governanceContract.getProposal(1);
+    const proposal = await governance.proposals(proposalId);
 
-    assert.equal(proposal.metadataCID, metadataCID);
-    assert.equal(proposal.startTime, startTime);
-    assert.equal(proposal.endTime, endTime);
-    assert.equal(proposal.yesVotes, 0);
-    assert.equal(proposal.noVotes, 0);
+    assert.equal(proposal.metadataCID, 'Proposal 1', 'Incorrect metadata CID');
+    assert.equal(proposal.startTime.toNumber(), startTime, 'Incorrect start time');
+    assert.equal(proposal.endTime.toNumber(), endTime, 'Incorrect end time');
+    assert.equal(proposal.yesVotes.toNumber(), 0, 'Incorrect initial yes votes');
+    assert.equal(proposal.noVotes.toNumber(), 0, 'Incorrect initial no votes');
   });
 
-  it('should not allow a member without role 1 to create a proposal', async () => {
-    const metadataCID = 'proposal-metadata';
-    const startTime = Math.floor(Date.now() / 1000); // Current timestamp
-    const endTime = startTime + 3600; // 1 hour from the start time
+  it('should vote on a proposal', async () => {
+    const startTime = Math.floor(Date.now() / 1000);
+    const endTime = startTime + 86400; // 1 day
 
-    try {
-      await governanceContract.createProposal(metadataCID, startTime, endTime, { from: memberWithRole2 });
-      assert.fail('Expected an exception');
-    } catch (error) {
-      assert.include(error.message, 'RoleBasedGovernance: Caller does not have the required role');
-    }
+    await governance.createProposal('Proposal 1', startTime, endTime);
+    const proposalId = await governance.proposalCount();
+
+    const member1 = accounts[1];
+    const member2 = accounts[2];
+
+    await governance.setMemberRole(member1, 1);
+    await governance.setMemberRole(member2, 2);
+
+    await governance.vote(proposalId, true, { from: member1 });
+    await governance.vote(proposalId, false, { from: member2 });
+
+    const proposal = await governance.proposals(proposalId);
+
+    assert.equal(proposal.yesVotes.toNumber(), 10, 'Incorrect yes votes');
+    assert.equal(proposal.noVotes.toNumber(), 21, 'Incorrect no votes');
   });
 
-  // Add more test cases as needed
+  it('should get active proposal IDs', async () => {
+    const startTime1 = Math.floor(Date.now() / 1000);
+    const endTime1 = startTime1 + 86400; // 1 day
+
+    const startTime2 = endTime1 + 86400; // 1 day
+    const endTime2 = startTime2 + 86400; // 1 day
+
+    await governance.createProposal('Proposal 1', startTime1, endTime1);
+    await governance.createProposal('Proposal 2', startTime2, endTime2);
+
+    const activeProposalIds = await governance.getActiveProposalIDs();
+
+    assert.equal(activeProposalIds.length, 1, 'Incorrect number of active proposals');
+    assert.equal(activeProposalIds[0].toNumber(), 1, 'Incorrect active proposal ID');
+  });
 });
